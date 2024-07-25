b24919e2d0deae6654491ca5835548d851c5dd05
bblackyyyy <dragochannel.vlad333.vlad@gmail.com>
2024-07-26 00:36:14 +0200

    executable file

---
+++ course/calculator/build.rs
@@ -0,0 +1,11 @@
+#![windows_subsystem = "windows"]
+extern crate winres;
+
+fn main(){
+    if cfg!(target_os = "windows") {
+        let mut res = winres::WindowsResource::new();
+        res.set_icon("icon.ico");
+        res.compile().unwrap();
+    }
+
+}
--- course/calculator/src/main.rs
+++ course/calculator/src/main.rs
@@ -1,3 +1,4 @@
+
 use std::cell::RefCell;
 use std::rc::Rc;
 use slint::private_unstable_api::re_exports::MouseCursor::Default;
@@ -96,60 +97,50 @@ struct Calculator{
 
 
 fn main() {
+
     let app: App = App::new().unwrap();
     let weak: Weak<App> = app.as_weak();
     let state = Rc::new(RefCell::new(Calculator::default()));
-    app.global::<CalcLogic>().on_button_pressed(move | counter | {
+    app.global::<CalcLogic>().on_button_pressed(move |counter| {
+        let app = weak.unwrap();
+        let mut state = state.borrow_mut();
 
-            let app = weak.unwrap();
-            let  mut state = state.borrow_mut();
+        if let Ok(counter) = counter.parse::<i32>() {
+            state.current_num *= 10;
+            state.current_num += counter;
+            app.set_counter(state.current_num);
+            return;
+        }
+        if counter.as_str() == "=" {
+            let result = match state.operator.as_str() {
+                "+" => state.prev_num + state.current_num,
+                "-" => state.prev_num - state.current_num,
+                "*" => state.prev_num * state.current_num,
+                "/" => state.prev_num / state.current_num,
 
-             if let Ok(counter) = counter.parse::<i32>(){
-                 state.current_num *=10;
-                    state.current_num += counter;
-                 app.set_counter(state.current_num);
-                 return;
+                _ => return
+            };
+            app.set_counter(result);
 
-             }
-            if counter.as_str() == "="{
-                let result = match state.operator.as_str(){
-                    "+" => state.prev_num + state.current_num,
-                    "-" => state.prev_num - state.current_num,
-                    "*" => state.prev_num * state.current_num,
-                    "/" => state.prev_num / state.current_num,
-
-
-                    _ => return
-                };
-                app.set_counter(result);
-
-                state.prev_num = result;
-                state.current_num = 0;
-                state.operator =slint::SharedString::new();;  ; //std::default::Default::default();
-
-                }
-
-                if counter.as_str() == "C"{
-                    state.prev_num = 0;
-                    state.current_num = 0;
-                    state.operator = slint::SharedString::new();;
-                    app.set_counter(0);
-                    return;
-                }
-        else {
-            state.operator = counter.clone();
-
-            state.prev_num =  app.get_counter();
-            state.current_num = 0;;
+            state.prev_num = result;
+            state.current_num = 0;
+            state.operator = slint::SharedString::new();
         }
 
+        if counter.as_str() == "C" {
+            state.prev_num = 0;
+            state.current_num = 0;
+            state.operator = slint::SharedString::new();
+            app.set_counter(0);
+            return;
+        } else {
+            state.operator = counter.clone();
 
-            }
-
-
-
+            state.prev_num = app.get_counter();
+            state.current_num = 0;
+        }
+    }
     );
-
 
 
     app.run().unwrap();
